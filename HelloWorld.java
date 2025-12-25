6be2884f0a3a017bacfe6589bf4e709fcfa33e74
srinfotechbatch4 <srinfotechbatch4@gmail.com>
2025-12-25 10:43:22 +0530

    updated hello java class

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,3 +3,15 @@ public class HelloWorld {
     System.out.println("Hello world!");
   }
 }
+
+public class SRINFOETCH {
+  public static void main(String[] args) {
+    System.out.println("Hello SRINFOETCH!");
+  }
+}
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello Batch5!");
+  }
+}
--- README.md
+++ README.md
@@ -14,6 +14,8 @@ public class HelloWorld {
 }
 ```
 
+
+
 Notice that `System.out.println("Hello world!");` shows the string `"Hello world!"` on the screen.
 
 ## Compile program
